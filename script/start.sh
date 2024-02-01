29f7c8eeb297603dd32c5048ca8f72cf29ad1a1e
karthrand <735867550@qq.com>
2024-02-01 23:40:57 +0800

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -2,7 +2,7 @@
 ENV_PROJECT_PATH=$1
 
 #启动flask服务
-#nohup python3 ${ENV_PROJECT_PATH}/main.py > output.log 2>&1 &
+nohup python3 ${ENV_PROJECT_PATH}/main.py > output.log 2>&1 &
 
 #启动sshd服务
 /usr/sbin/sshd -d
