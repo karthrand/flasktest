f37367592dcef6a458b4afff1e88cfec6fa855ed
codeserver <735867550@qq.com>
2024-02-01 19:01:53 +0800

    镜像构建

--- script/start.sh
+++ script/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ENV_PROJECT_PATH=$1
 
-#启动sshd服务
-/usr/sbin/sshd &
 #启动flask服务
-python3 ${ENV_PROJECT_PATH}/main.py
+nohup python3 ${ENV_PROJECT_PATH}/main.py > output.log 2>&1 &
+
+#启动sshd服务
+/usr/sbin/sshd -d
 
 #等待回调执行完，主进程再退出
 wait
